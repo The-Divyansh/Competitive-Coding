5113cb68c44538ed3f62d10897c886e85492b0b4
The-Divyansh <divyansh.coding.0602@gmail.com>
2021-12-05 19:58:58 +0530

    3rd question

---
+++ CSES/Introductory Problems/Repetitions.cpp
@@ -0,0 +1,67 @@
+// ████████▄   ▄█   ▄█    █▄  ▄██   ▄      ▄████████ ███▄▄▄▄      ▄████████    ▄█    █▄
+// ███   ▀███ ███  ███    ███ ███   ██▄   ███    ███ ███▀▀▀██▄   ███    ███   ███    ███
+// ███    ███ ███▌ ███    ███ ███▄▄▄███   ███    ███ ███   ███   ███    █▀    ███    ███
+// ███    ███ ███▌ ███    ███ ▀▀▀▀▀▀███   ███    ███ ███   ███   ███         ▄███▄▄▄▄███▄▄
+// ███    ███ ███▌ ███    ███ ▄██   ███ ▀███████████ ███   ███ ▀███████████ ▀▀███▀▀▀▀███▀
+// ███    ███ ███  ███    ███ ███   ███   ███    ███ ███   ███          ███   ███    ███
+// ███   ▄███ ███  ███    ███ ███   ███   ███    ███ ███   ███    ▄█    ███   ███    ███
+// ████████▀  █▀    ▀██████▀   ▀█████▀    ███    █▀   ▀█   █▀   ▄████████▀    ███    █▀
+
+// #pragma GCC optimize "trapv"
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ff first
+#define endl '\n'
+#define ss second
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define fo(i, a, b) for (int i = a; i < b; i++)
+#define foe(i, a, b) for (int i = a; i <= b; i++)
+
+const int MOD1 = 998244353;
+const int MOD = 1000000007;
+
+typedef long long int ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> pll;
+typedef map<int, int> mii;
+typedef pair<int, int> pii;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    //#ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    //#endif
+
+    string s;
+    cin >> s;
+    int n = s.size();
+    char curr = s[0];
+    int ans = 1;
+    int currLen = 1;
+    fo(i, 1, n)
+    {
+        if (s[i] == s[i - 1])
+        {
+            currLen++;
+            ans = max(ans, currLen);
+        }
+        else
+        {
+            currLen = 1;
+            curr = s[i];
+        }
+    }
+    cout << ans << endl;
+
+    return 0;
+}
