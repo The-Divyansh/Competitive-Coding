cd844c2423add536dc2122e311a2742e463ec144
The-Divyansh <divyansh.coding.0602@gmail.com>
2021-12-05 20:14:48 +0530

    Added 4th and 5th questions

---
+++ CSES/Introductory Problems/Permutations.cpp
@@ -0,0 +1,62 @@
+// ████████▄   ▄█   ▄█    █▄  ▄██   ▄      ▄████████ ███▄▄▄▄      ▄████████    ▄█    █▄
+// ███   ▀███ ███  ███    ███ ███   ██▄   ███    ███ ███▀▀▀██▄   ███    ███   ███    ███
+// ███    ███ ███▌ ███    ███ ███▄▄▄███   ███    ███ ███   ███   ███    █▀    ███    ███
+// ███    ███ ███▌ ███    ███ ▀▀▀▀▀▀███   ███    ███ ███   ███   ███         ▄███▄▄▄▄███▄▄
+// ███    ███ ███▌ ███    ███ ▄██   ███ ▀███████████ ███   ███ ▀███████████ ▀▀███▀▀▀▀███▀
+// ███    ███ ███  ███    ███ ███   ███   ███    ███ ███   ███          ███   ███    ███
+// ███   ▄███ ███  ███    ███ ███   ███   ███    ███ ███   ███    ▄█    ███   ███    ███
+// ████████▀  █▀    ▀██████▀   ▀█████▀    ███    █▀   ▀█   █▀   ▄████████▀    ███    █▀
+
+// #pragma GCC optimize "trapv"
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ff first
+#define endl '\n'
+#define ss second
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define fo(i, a, b) for (int i = a; i < b; i++)
+#define foe(i, a, b) for (int i = a; i <= b; i++)
+
+const int MOD1 = 998244353;
+const int MOD = 1000000007;
+
+typedef long long int ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> pll;
+typedef map<int, int> mii;
+typedef pair<int, int> pii;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    //#ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    //#endif
+
+    int n;
+    cin >> n;
+
+    if (n == 1)
+        cout << 1 << endl;
+    else if (n == 2 || n == 3)
+        cout << "NO SOLUTION" << endl;
+    else
+    {
+        for (int i = 2; i <= n; i += 2)
+            cout << i << " ";
+        for (int i = 1; i <= n; i += 2)
+            cout << i << " ";
+        cout << endl;
+    }
+
+    return 0;
+}
