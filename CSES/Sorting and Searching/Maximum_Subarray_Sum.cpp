21bf744fe20837ee4cebc40b37e69fdaf37927c0
The-Divyansh <divyansh.coding.0602@gmail.com>
2021-12-06 16:51:34 +0530

    Kadane's Algorithm

---
+++ CSES/Sorting and Searching/Maximum_Subarray_Sum.cpp
@@ -0,0 +1,62 @@
+// ████████▄   ▄█   ▄█    █▄  ▄██   ▄      ▄████████ ███▄▄▄▄      ▄████████    ▄█    █▄
+// ███   ▀███ ███  ███    ███ ███   ██▄   ███    ███ ███▀▀▀██▄   ███    ███   ███    ███
+// ███    ███ ███▌ ███    ███ ███▄▄▄███   ███    ███ ███   ███   ███    █▀    ███    ███
+// ███    ███ ███▌ ███    ███ ▀▀▀▀▀▀███   ███    ███ ███   ███   ███         ▄███▄▄▄▄███▄▄
+// ███    ███ ███▌ ███    ███ ▄██   ███ ▀███████████ ███   ███ ▀███████████ ▀▀███▀▀▀▀███▀
+// ███    ███ ███  ███    ███ ███   ███   ███    ███ ███   ███          ███   ███    ███
+// ███   ▄███ ███  ███    ███ ███   ███   ███    ███ ███   ███    ▄█    ███   ███    ███
+// ████████▀  █▀    ▀██████▀   ▀█████▀    ███    █▀   ▀█   █▀   ▄████████▀    ███    █▀
+
+// #pragma GCC optimize "trapv"
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ff first
+#define endl '\n'
+#define ss second
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define fo(i, a, b) for (int i = a; i < b; i++)
+#define foe(i, a, b) for (int i = a; i <= b; i++)
+
+const int MOD1 = 998244353;
+const int MOD = 1000000007;
+
+typedef long long int ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> pll;
+typedef map<int, int> mii;
+typedef pair<int, int> pii;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    //#ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    //#endif
+
+    int n;
+    cin >> n;
+    vi v(n);
+    fo(i, 0, n) cin >> v[i];
+    ll max_so_far = LLONG_MIN;
+    ll curr_max = 0;
+    fo(i, 0, n)
+    {
+        curr_max = curr_max + v[i];
+        if (max_so_far < curr_max)
+            max_so_far = curr_max;
+        if (curr_max < 0)
+            curr_max = 0;
+    }
+    cout << max_so_far << endl;
+
+    return 0;
+}
